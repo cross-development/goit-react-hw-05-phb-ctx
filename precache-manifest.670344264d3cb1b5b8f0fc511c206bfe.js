e3c437726eb26fff342dcadd60a1a2e2c36ccd94
Vitaliy Derda <derdavitaliy@gmail.com>
2020-07-19 19:05:30 +0300

    Updates

---
+++ precache-manifest.670344264d3cb1b5b8f0fc511c206bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115a3e361cebaa1af968c30e24b64a90",
+    "url": "/goit-react-hw-05-phb-ctx/index.html"
+  },
+  {
+    "revision": "9d8670b4c35e958e2403",
+    "url": "/goit-react-hw-05-phb-ctx/static/css/main.60ac6005.chunk.css"
+  },
+  {
+    "revision": "289f8afc54e5bbc4823e",
+    "url": "/goit-react-hw-05-phb-ctx/static/js/2.5adba5de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phb-ctx/static/js/2.5adba5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8670b4c35e958e2403",
+    "url": "/goit-react-hw-05-phb-ctx/static/js/main.8f0a6991.chunk.js"
+  },
+  {
+    "revision": "389917a9060ef5ef8b98",
+    "url": "/goit-react-hw-05-phb-ctx/static/js/runtime-main.79fd74a5.js"
+  }
+]);
